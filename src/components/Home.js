4184521b2584dbc220b3dc5d5e73ba76b027bdd6
robertalanis <rxalanis@gmail.com>
2020-07-29 01:08:12 -0600

    added useHomeFetch hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from "../config";
 
-//import components
+// Import components
 import HeroImage from "./elements/HeroImage";
 import SearchBar from "./elements/SearchBar";
 import Grid from "./elements/Grid";
@@ -8,15 +9,23 @@ import MovieThumb from "./elements/MovieThumb";
 import LoadMoreBtn from "./elements/LoadMoreBtn";
 import Spinner from "./elements/Spinner";
 
-const Home = () => (
-	<>
-		<HeroImage />
-		<SearchBar />
-		<Grid />
-		<MovieThumb />
-		<LoadMoreBtn />
-		<Spinner />
-	</>
-);
+// Custom Hook
+import { useHomeFetch } from "./hooks/useHomeFetch";
+
+const Home = () => {
+    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+    console.log(state);
+
+	return (
+		<>
+			<HeroImage />
+			<SearchBar />
+			<Grid />
+			<MovieThumb />
+			<LoadMoreBtn />
+			<Spinner />
+		</>
+	);
+};
 
 export default Home;
