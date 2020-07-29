196468d44d77980789d821991f5671ca2cc80a28
robertalanis <rxalanis@gmail.com>
2020-07-29 12:08:59 -0600

    added and styled heroimage and spinner

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
+import { createGlobalStyle } from "styled-components";
 
 import Header from "./elements/Header";
 import Home from "./Home";
 
+import { GlobalStyle } from "./styles/GlobalStyle";
+
 const App = () => (
-	<div>
+	<>
 		<Header />
-        <Home />
-	</div>
+		<Home />
+		<GlobalStyle />
+	</>
 );
 
 export default App;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,12 @@
-import React, { useState, useEffect } from "react";
-import { API_URL, API_KEY, API_BASE_URL, POSTER_SIZE, BACKDROP_SIZE } from "../config";
+import React, { useState } from "react";
+import {
+	API_URL,
+	API_KEY,
+	API_BASE_URL,
+	POSTER_SIZE,
+	BACKDROP_SIZE,
+	IMAGE_BASE_URL,
+} from "../config";
 
 // Import components
 import HeroImage from "./elements/HeroImage";
@@ -12,13 +19,24 @@ import Spinner from "./elements/Spinner";
 // Custom Hook
 import { useHomeFetch } from "./hooks/useHomeFetch";
 
+import NoImage from "./images/no_image.jpg";
+
 const Home = () => {
-    const [{ state, loading, error }, fetchMovies] = useHomeFetch();
-    console.log(state);
+	const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+	const [searchTerm, setSearchTerm] = useState("");
+
+	console.log(state);
+
+	if (error) return <div>Somethign went wrong...</div>;
+	if (!state.movies[0]) return <Spinner />;
 
 	return (
 		<>
-			<HeroImage />
+			<HeroImage
+				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
+				title={state.heroImage.original_title}
+				text={state.heroImage.overview}
+			/>
 			<SearchBar />
 			<Grid />
 			<MovieThumb />
