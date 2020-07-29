ae1c04b75a422b5bc0a6a44f54d414361c22397d
robertalanis <rxalanis@gmail.com>
2020-07-29 12:34:57 -0600

    added and styled movie thumbsnails

--- src/components/Home.js
+++ src/components/Home.js
@@ -22,23 +22,42 @@ import { useHomeFetch } from "./hooks/useHomeFetch";
 import NoImage from "./images/no_image.jpg";
 
 const Home = () => {
-	const [{ state, loading, error }, fetchMovies] = useHomeFetch();
+	const [
+        { 
+            state: {movies, currentPage, totalPages, heroImage}, 
+            loading, 
+            error 
+        }, 
+        fetchMovies
+    ] = useHomeFetch();
 	const [searchTerm, setSearchTerm] = useState("");
 
-	console.log(state);
-
 	if (error) return <div>Somethign went wrong...</div>;
-	if (!state.movies[0]) return <Spinner />;
+	if (!movies[0]) return <Spinner />;
 
 	return (
 		<>
 			<HeroImage
-				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.heroImage.backdrop_path}`}
-				title={state.heroImage.original_title}
-				text={state.heroImage.overview}
+				image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${heroImage.backdrop_path}`}
+				title={heroImage.original_title}
+				text={heroImage.overview}
 			/>
 			<SearchBar />
-			<Grid />
+			<Grid header={searchTerm ? "Search Result" : "Popular Movies"}>
+				{movies.map((movie) => (
+					<MovieThumb
+						key={movie.id}
+						clickable
+						image={
+							movie.poster_path
+								? `${IMAGE_BASE_URL}${BACKDROP_SIZE}${movie.poster_path}`
+								: NoImage
+						}
+						movieID={movie.id}
+						movieName={movie.original_title}
+					/>
+				))}
+			</Grid>
 			<MovieThumb />
 			<LoadMoreBtn />
 			<Spinner />
--- src/components/elements/MovieThumb.js
+++ src/components/elements/MovieThumb.js
@@ -1,5 +1,15 @@
 import React from "react";
 
-const MovieThumb = () => <div>MovieThumb</div>;
+import { StyledMovieThumb } from "../styles/StyledMovieThumb"
+
+const MovieThumb = ({ image, movieID, clickable}) => (
+    <StyledMovieThumb>
+        {clickable ? (
+            <img className="clckable" src={image} alt="moviethumb" />
+        ) : (
+            <img src={image} alt="moviethumb" />
+        )}
+    </StyledMovieThumb>
+);
 
 export default MovieThumb;
