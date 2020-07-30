85951b8a76de1b7a129d4c19f4795d472e51ac3b
robertalanis <rxalanis@gmail.com>
2020-07-29 22:53:52 -0600

    Updates

---
+++ precache-manifest.3a98d148d4f0bd2019ab375df23e22e0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c130b9b54efe7e8eadb6cd40c76aed79",
+    "url": "/react-rmdb/index.html"
+  },
+  {
+    "revision": "64a9d2d4ee3cb147d1e3",
+    "url": "/react-rmdb/static/js/2.c3248a92.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-rmdb/static/js/2.c3248a92.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4b7fc4408423b9d044b",
+    "url": "/react-rmdb/static/js/main.96d0c09e.chunk.js"
+  },
+  {
+    "revision": "b1ee5361bf9dc9dcd2d9",
+    "url": "/react-rmdb/static/js/runtime-main.0cdc6992.js"
+  },
+  {
+    "revision": "31f5bb1da5a478ef98a9213900c5f7a2",
+    "url": "/react-rmdb/static/media/no_image.31f5bb1d.jpg"
+  },
+  {
+    "revision": "08494abf4210d1ea8b163791db70773c",
+    "url": "/react-rmdb/static/media/reactMovie_logo.08494abf.png"
+  },
+  {
+    "revision": "30cd724bda43bdfa29216c8d8770b0c9",
+    "url": "/react-rmdb/static/media/tmdb_logo.30cd724b.svg"
+  }
+]);
