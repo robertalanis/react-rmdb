4ff8c1b745b5fd12a1686a7d7fafa862714e34df
robertalanis <rxalanis@gmail.com>
2020-07-30 20:05:52 -0600

    actor grid

--- src/components/elements/MovieInfoBar.js
+++ src/components/elements/MovieInfoBar.js
@@ -1,5 +1,29 @@
-import React from 'react';
+import React from "react";
+import FontAweseome from "react-fontawesome";
+import { calcTime, convertMoney } from "../../helpers";
 
-const MovieInfoBar = () => <div>Movie Info Bar</div>
- 
+import { StyledMovieInfoBar } from "../styles/StyledMovieInfoBar";
+import FontAwesome from "react-fontawesome";
+
+const MovieInfoBar = ({ time, budget, revenue }) => (
+	<StyledMovieInfoBar>
+		<div className="movieinfobar-content">
+			<div className="movieinfobar-content-col">
+				<FontAwesome classID="fa-time" name="clock-o" size="2x" />
+				<span className="movieinfobar-info">Running time: {calcTime(time)}</span>
+			</div>
+
+			<div className="movieinfobar-content-col">
+				<FontAwesome classID="fa-budget" name="money" size="2x" />
+				<span className="movieinfobar-info">Budget: {convertMoney(budget)}</span>
+			</div>
+
+			<div className="movieinfobar-content-col">
+				<FontAwesome classID="fa-revenue" name="ticket" size="2x" />
+				<span className="movieinfobar-info">Revenue: {convertMoney(revenue)}</span>
+			</div>
+		</div>
+	</StyledMovieInfoBar>
+);
+
 export default MovieInfoBar;
